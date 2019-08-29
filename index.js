7a88074ef5486b39ad6b8c38673b4ff9c3fb6e37
Taiju Aoki <aokitaiju0513@gmail.com>
2019-08-30 00:40:36 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 200,
+    x: 0,
+    y: 0,
+    // opacity: 0.5,
+    movable: true,
+    frame: false,
+    resizable: true,
+    hasShadow: false,
+    alwaysOnTop: true,
+    titleBarStyle: 'customButtonOnHover',
+    webPreferences: {
+      // nodeIntegration: true,
+      webviewTag: true
+    }
+  });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
